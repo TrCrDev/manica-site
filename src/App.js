ba5105b8b8aee7a263cea79ac04504358189fb99
ashazacdev <145595196+ashazacdev@users.noreply.github.com>
2023-12-03 23:31:20 +0100

    1st-commit

---
+++ src/App.js
@@ -0,0 +1,251 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        <h1>Manica Couture</h1>
+        <h2>LYON</h2>
+      </div>
+      <div className="main-content">
+        <h3>CREATRICE DE ROBES DE MARIEE SUR MESURE</h3>
+        <p>L’univers du rêve et de l’élégance,</p>
+        <p>Où chaque robe est une œuvre d’art conçue spécialement pour vous.</p>
+      </div>
+      <div className="line-container">
+        <div className="right-line"></div>
+      </div>
+
+
+
+      <div className="concept-section">
+        <h3>NOTRE CONCEPT</h3>
+        <p className="slogan">“UN SAVOIR FAIRE SUR MESURE”</p>
+        <p className="description">
+          MANICA vous propose des créations de robes de mariée haut de gamme, à l’imaginaire infini. <br></br>
+          Nous transformons vos idées, vos rêves et inspirations en des styles uniques qui reflètent votre personnalité. <br></br>
+          Votre robe de mariée devient une réalité sur mesure, conçue spécialement pour vous.
+        </p>
+        <img src="1.png" alt="" className="centered-image" />
+      </div>
+
+
+
+
+      <div className="concept-section">
+        <h3>UN SERVICE HAUT DE GAMME</h3>
+        <p className="slogan">“UNE ÉTROITE COLLABORATION”...</p>
+        <p className="description">
+        Au delà de la couture, c’est une vraie relation que nous partageons avec nos futures mariées, 
+        guidées par l'engagement et le désir de créer la robe parfaite pour votre mariage.
+        </p>
+        <img src="2.png" alt="" className="centered-image" />
+      </div>
+
+
+
+
+      <div className="rdv-section">
+        <img src="rdv-title.svg" alt="" className="rdv-image" />
+        <p className="description">
+          Notre premier rendez-vous téléphonique nous permettra grâce à un échange intense de vous proposer <br></br>
+          les premières bases de la conception de votre robe et de programmer une rencontre au show room.
+        </p>
+      </div>
+
+
+      <div className="showroom-section">
+        <img src="showroom-title.svg" alt="" className="showroom-image" />
+        <p className="description">
+        Ensemble, nous sélectionnerons les matières et prendrons vos mesures. <br></br>
+        Il est essentiel pour nous d'accorder du temps et de l'importance aux moindres détails, créant ainsi une pièce d'exception.
+        </p>
+      </div>
+
+      <div className="img-section">
+        <img src="3.png" alt="" className="centered-image"/>
+      </div>
+
+     
+      <div className="concept-section">
+        <h3>AU SERVICE DE LA MARIÉE</h3>
+        <p className="slogan">“UNE RENCONTRE QUI CRÉE DU LIEN”</p>
+        <p className="description">
+        Tout au long du processus de la création de votre robe, 
+        nous vous tiendrons informé à chaque étape, 
+        et vous offrons la possibilité de la suivre visuellement en temps réel, 
+        garantissant ainsi une expérience transparente et personnalisée. 
+
+        </p>
+        <img src="4.png" alt="" className="centered-image" />
+      </div>
+
+
+
+
+      <div className="concept-section">
+        <h3>DE LA QUALITÉ</h3>
+        <p className="slogan">“DÉTAILS, EXCELLENCE, HAUTE COUTIURE”</p>
+        <p className="description">
+        L’excellence que nous mettons à votre service se manifeste à travers 
+        la sélection minutieuse et soignée de nos tissus, dentelles, perles et ornements. <br></br><br></br>
+        Chaque robe, une oeuvre perlée à la main, pour une élégance unique et artisanale.  
+        </p>
+        <img src="5.png" alt="" className="centered-image" />
+        <br></br><br></br>
+        <img src="6.png" alt="" className="centered-image" />
+      </div>
+
+
+
+      <div className="concept-section">
+        <h3>LA CRÉATRICE</h3>
+        <img src="7.png" alt="" className="centered-image" />
+        <p className="description">
+        Après plusieurs années dédiées à sublimer la mariée, et animée par ma passion pour les robes parées de pierres et de lumières, 
+        j'ai développé un savoir-faire et une expérience qui me permet de satisfaire nos clientes et de contribuer au plus beau jour de leur vie.<br></br>
+
+        On prend vos idées, vos rêves et vos inspirations, et je donnerai vie à votre robe pour qu’elle soit le reflet de votre personnalité.<br></br><br></br>
+
+        Située sur la région Lyonnaise, je vous accueille dans un cadre chaleureux et convivial où mon soucis du détail 
+        et mon professionnalisme nous permettrons de concrétiser votre projet. <br></br><br></br> 
+
+        Votre satisfaction est ma plus grande récompense, laissez moi vous accompagnez dans cette aventure.
+        </p>
+
+        <img src="styled-text.svg" alt="" className="centered-image" />
+       
+      </div>
+
+
+
+
+      <div className="concept-section">
+        <h3>NOS MARIÉS</h3>
+        <img src="8.png" alt="" className="centered-image" />
+
+
+        <img src="ny.svg" alt="" className="small-centered-image" />
+        <p className="description">
+        MANICA c’est avant tout une très belle rencontre...
+        Passionnée par son métier et ultra douée pour donner vie à la robe que j’avais imaginé.<br></br>
+        Des échanges et conseils sur la création, en passant par la prise de mesures, le suivi de la fabrication à l’atelier, jusqu’à l’essayage, tout à été parfait !<br></br>
+        Osez franchir le pas de la création et personnalisation, votre robe sera unique comme cette journée que vous apprêtez à vivre !
+        Je la recommande les yeux fermés ! 
+        </p>
+        <img src="9.png" alt="" className="centered-image" />
+
+
+        <img src="yv.svg" alt="" className="small-centered-image" />
+        <p className="description">
+        Un grand MERCI à MANICA d’avoir réalisé ma magnifique robe de mariée sur mesure et personnalisée.<br></br>
+        Un accompagnement au top, de la confection de la robe, du choix du tissu jusqu’au jour du mariage, j’ai eu de la chance de l’avoir à mes côtés.<br></br>
+        Ma robe a fait son effet et j’en suis encore très reconnaissante.
+        Un énorme MERCI MANICA ! 
+        </p>
+        <img src="10.png" alt="" className="centered-image" />
+
+
+        <img src="my.svg" alt="" className="small-centered-image" />
+        <p className="description">
+        Un grand MERCI à MANICA d’avoir réalisé ma magnifique robe de mariée sur mesure et personnalisée.<br></br>
+        Un accompagnement au top, de la confection de la robe, du choix du tissu jusqu’au jour du mariage, j’ai eu de la chance de l’avoir à mes côtés.<br></br>
+        Ma robe a fait son effet et j’en suis encore très reconnaissante.
+        Un énorme MERCI MANICA ! 
+        </p>
+        <img src="11.png" alt="" className="centered-image" />
+
+
+
+        <img src="la.svg" alt="" className="small-centered-image" />
+        <p className="description">
+        MANICA c’est avant tout une très belle rencontre...
+        Passionnée par son métier et ultra douée pour donner vie à la robe que j’avais imaginé.<br></br>
+        Des échanges et conseils sur la création, en passant par la prise de mesures, le suivi de la fabrication à l’atelier, jusqu’à l’essayage, tout à été parfait !<br></br>
+        Osez franchir le pas de la création et personnalisation, votre robe sera unique comme cette journée que vous apprêtez à vivre !
+        Je la recommande les yeux fermés ! 
+        </p>
+        <img src="12.png" alt="" className="centered-image" />
+
+
+
+        <img src="lo.svg" alt="" className="small-centered-image" />
+        <p className="description">
+        Pas de mots, simplementt et clairement incroyable !!
+        Je n’ai pas hésité à faire confectionner ma robe de mariée sirène sur mesure par MANICA.<br></br>
+        Son service irréprochable, sa bienveillance très appréciée et ses rassurants conseils m’ont permis de voir venir le jour J arriver avec plus de confiance !<br></br>
+        Merci pour tout, je recommande ses services.<br></br>
+        Je suis venue de LAUSANNE sans regret !
+        </p>
+        <img src="13.png" alt="" className="centered-image" />
+
+
+
+
+        <img src="ju.svg" alt="" className="small-centered-image" />
+        <p className="description">
+        Je recommande MANICA à 1000% la meilleure créatrice de robes de mariée.<br></br>
+        Les matières et les détails de ma robe sont exceptionnels !<br></br>
+        Merci pour tout ! 
+        </p>
+        <img src="14.png" alt="" className="centered-image" />
+
+
+
+
+        <img src="el.svg" alt="" className="small-centered-image" />
+        <p className="description">
+        J’ai  fais appel aux services de MANICA pour ma robe de mariée sur mesure.<br></br>
+        J’ai été très satisfaite. <br></br>
+        J’ai eu la robe de princesse que je voulais tant.<br></br>
+        MANICA est très professionnelle et à l’écoute, elle saura satisfaire vos exigences.<br></br>
+        Je la recommande ! 
+        </p>
+
+
+      </div>
+
+      <div className="concept-section">
+        <h3>CONTACT</h3>
+        <p className="description">
+          Nous mettons tout en œuvre pour répondre rapidement à votre demande. 
+          N'hésitez pas à nous contacter pour toute question, chaque demande est importante pour nous.
+        </p>
+        <div className="contact-icons">
+          <div className="icon">
+            <img src="whatsapp.png" alt="Whatsapp" />
+            <p><strong>Whatsapp</strong></p>
+          </div>
+          <div className="icon">
+            <img src="mail.png" alt="Email" />
+            <p><strong>Email</strong></p>
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+      <div className="concept-section">
+        <h3>SUIVEZ-NOUS</h3>
+        <div className="contact-icons">
+          <div className="icon">
+            <img src="instagram.png" alt="Instagram" />
+          </div>
+          <div className="icon">
+            <img src="facebook.png" alt="Facebook" />
+          </div>
+        </div>
+      </div>
+
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
