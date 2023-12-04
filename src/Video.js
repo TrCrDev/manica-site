ae26f54e845dae57407eb94bd1a7c299a65e4fe7
ashazacdev <145595196+ashazacdev@users.noreply.github.com>
2023-12-04 21:30:21 +0100

    .

--- src/Video.js
+++ src/Video.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './Video.css';
 
 const VideoComponent = () => {
-
   return (
     <div className="video-container">
       <video autoPlay loop muted className="background-video">
