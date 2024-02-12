3ef21f77d9abfb7084d71306402637d7cf913dbd
TrCrDev <wasstrcr@proton.me>
2024-02-12 10:52:23 +0100

    updated video logic

--- src/Video.js
+++ src/Video.js
@@ -2,25 +2,31 @@ import React, { useEffect, useRef } from 'react';
 import './Video.css';
 
 const VideoComponent = () => {
-  const videoRef = useRef(null);
+  const videoRef = useRef();
 
   useEffect(() => {
-    if (videoRef.current) {
-      videoRef.current.play().catch(error => console.log("Error attempting to play video: ", error));
-    }
-  }, []);
+    const attemptPlay = async () => {
+      try {
+        await videoRef.current.play();
+      } catch (err) {
+        console.error('Failed to play the video automatically:', err);
+      }
+    };
 
+    if (videoRef.current) {
+      attemptPlay();
+    }
+  }, []); 
   return (
     <div className="video-container">
       <video
         ref={videoRef}
         loop
-        autoPlay
-        playsInline
         muted
+        playsInline
         preload="auto"
-        title="Local Video"
         className="background-video"
+        style={{ width: '100%', height: 'auto', objectFit: 'cover' }} 
       >
         <source src="video.mp4" type="video/mp4" />
       </video>
