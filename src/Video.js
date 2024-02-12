dedc253a13ab865a8b07a49455e570d3dc4cfee0
TrCrDev <wasstrcr@proton.me>
2024-02-12 11:03:00 +0100

    testing video play on prod

--- src/Video.js
+++ src/Video.js
@@ -2,21 +2,24 @@ import React, { useEffect, useRef } from 'react';
 import './Video.css';
 
 const VideoComponent = () => {
-  const videoRef = useRef();
+  const videoRef = useRef(null);
 
   useEffect(() => {
-    const attemptPlay = async () => {
-      try {
-        await videoRef.current.play();
-      } catch (err) {
-        console.error('Failed to play the video automatically:', err);
-      }
-    };
-
     if (videoRef.current) {
-      attemptPlay();
+
+      videoRef.current.onloadeddata = null;
+      videoRef.current.onerror = null;
+
+      videoRef.current.onloadeddata = () => {
+        videoRef.current.play().catch(err => console.error('Error playing video:', err));
+      };
+
+      videoRef.current.onerror = () => {
+        console.error('Error loading video');
+      };
     }
-  }, []); 
+  }, [videoRef]); 
+
   return (
     <div className="video-container">
       <video
@@ -26,9 +29,9 @@ const VideoComponent = () => {
         playsInline
         preload="auto"
         className="background-video"
-        style={{ width: '100%', height: 'auto', objectFit: 'cover' }} 
       >
         <source src="video.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
       </video>
     </div>
   );
