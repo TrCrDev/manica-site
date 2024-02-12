baac53d043f1cd49d93e609798e57e2b370b497c
TrCrDev <wasstrcr@proton.me>
2024-02-12 10:43:06 +0100

    added trigger for video auto play

--- src/Video.js
+++ src/Video.js
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './Video.css';
 
 const VideoComponent = () => {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.play().catch(error => console.log("Error attempting to play video: ", error));
+    }
+  }, []);
+
   return (
     <div className="video-container">
-      <video         
+      <video
+        ref={videoRef}
         loop
         autoPlay
         playsInline
         muted
         preload="auto"
-        title="Local Video" 
-        className="background-video">
+        title="Local Video"
+        className="background-video"
+      >
         <source src="video.mp4" type="video/mp4" />
       </video>
     </div>
   );
 };
-
-
 
 export default VideoComponent;
