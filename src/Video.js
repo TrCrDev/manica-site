5cc5a35f3f39b68fea94868697f9a61185e26d7a
TrCrDev <wasstrcr@proton.me>
2024-02-12 11:04:39 +0100

    test video play on prod

--- src/Video.js
+++ src/Video.js
@@ -6,32 +6,23 @@ const VideoComponent = () => {
 
   useEffect(() => {
     if (videoRef.current) {
-
-      videoRef.current.onloadeddata = null;
-      videoRef.current.onerror = null;
-
-      videoRef.current.onloadeddata = () => {
-        videoRef.current.play().catch(err => console.error('Error playing video:', err));
-      };
-
-      videoRef.current.onerror = () => {
-        console.error('Error loading video');
-      };
+      videoRef.current.play().catch(error => console.log("Error attempting to play video: ", error));
     }
-  }, [videoRef]); 
+  }, []);
 
   return (
     <div className="video-container">
       <video
         ref={videoRef}
         loop
-        muted
+        autoPlay
         playsInline
+        muted
         preload="auto"
+        title="Local Video"
         className="background-video"
       >
         <source src="video.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
       </video>
     </div>
   );
